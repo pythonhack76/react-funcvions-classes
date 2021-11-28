a576fa18bdb81ab5b197c10466e61b66d4aaf0a4
Python The Hack <lrulvoni@gmail.com>
2021-11-28 12:56:18 +0100

    aggiunta useState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+const Todo = (props) => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+      {props.todo.name}
     </div>
-  );
+    
+  )
 }
+
+
+
+class App extends React.Component {
+  state = {
+    todos: [
+      { name: 'finire un lavoro', completato: true},
+      { name: 'farsi pagare', completato: true},
+      { name: 'finire un lavoro', completato: true},
+    ]
+  }
+
+    render() {
+      return (
+        <div className="app">
+          <div className="todo-list">
+            {this.state.todos.map((item, index) => {
+              <Todo key={index} todo={item} />
+            })}
+
+          </div>
+        </div>
+      )
+    }
+}
+ 
 
 export default App;
